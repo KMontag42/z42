4fccd6bd3d2022c649e1d3304a4fcd55f5d91766
Kyle Montag <montag.k@husky.neu.edu>
2013-02-11 23:35:10 -0500

    movement looking hot.

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -5,13 +5,24 @@ using System.Collections.Generic;
 public class Unit : MonoBehaviour
 {
     #region Fields
-    public int[] coordinates = new int[3];
-    public int[] moveTo = new int[3];
+private
+	float strength;
+	float intelligence;
+	float dexterity;
+	float vitality;
+	float move_range;
     #endregion
-
+	
+	
+	
     #region Methods
     // Use this for initialization
 	public virtual void Start () {
+		strength = 10;
+		intelligence = 10;
+		dexterity = 10;
+		vitality = 10;
+		move_range = 20;
 	}
 	
 	// Update is called once per frame
@@ -25,7 +36,7 @@ public class Unit : MonoBehaviour
 	{
 		Camera camera_3d = Camera.allCameras[0];
 		Vector3 pos = camera_3d.WorldToScreenPoint(transform.position);
-		GUIOverlay.showTooltip(this, pos.x, pos.y);
+		GUIOverlay.showTooltip(this, pos.x, Screen.height - pos.y);
 	}
 	
 	public IEnumerator Move ()
@@ -33,17 +44,30 @@ public class Unit : MonoBehaviour
 		print ("moving");
 		GameObject indicator = GameObject.Instantiate(Resources.Load("Prefabs/range_indicator") as GameObject, transform.position, transform.rotation) as GameObject;
 		Vector3 mouse_input = new Vector3(0,0,0);
+		Vector3 target_destination = new Vector3(0,0,0);
 		bool ready_to_move = false;
 		do
 		{
 			yield return StartCoroutine(GetInput());
 			mouse_input = Input.mousePosition;
-			ready_to_move = true;
+			
+			Plane playerPlane = new Plane(Vector3.up, transform.position);
+			Ray ray = Camera.allCameras[0].ScreenPointToRay(mouse_input);
+			float hitdist = 0.0f;
+ 
+			if (playerPlane.Raycast(ray, out hitdist)) {
+				target_destination = ray.GetPoint(hitdist);
+				Quaternion targetRotation = Quaternion.LookRotation(target_destination - transform.position);
+				transform.rotation = targetRotation;
+				ready_to_move = true;
+			}
+			
 		} while (!ready_to_move);
 		print ("moved");
-		print (mouse_input);
+		print (target_destination);
 		Destroy(indicator);
-		yield return null;
+		
+		yield return StartCoroutine(MoveTo (target_destination, 1));
 	}
 	
 	public IEnumerator GetInput()
